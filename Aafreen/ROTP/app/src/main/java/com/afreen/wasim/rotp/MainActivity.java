40a4d2d2a4512c1b746b768a021e46b049ccf98f
Aafreen-2901 <aafreenakhtar.af@gmail.com>
2017-07-02 14:03:30 +0530

    Upoading the Android project ROTP

---
+++ Aafreen/ROTP/app/src/main/java/com/afreen/wasim/rotp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.afreen.wasim.rotp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyView mv = new MyView(this);
+        setContentView(mv);
+
+    }
+}
